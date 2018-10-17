c86a1fb878b399ce29e3726d89be8f1d93832103
manolo <manoloesparta@gmail.com>
2018-10-17 10:29:14 -0700

    [update] Spotify delete duplicates

--- spotify/main.py
+++ spotify/main.py
@@ -38,6 +38,7 @@ results = Spotify.user_playlist_tracks(username, playlist_retrieved)
 tracks = results['items']
 
 while results['next']:
+
 	results = Spotify.next(results)
 	tracks.extend(results['items'])
 
@@ -45,27 +46,40 @@ albums_uri = []
 albums_name = []
 
 for i in tracks:
+
 	albums_uri.append(i['track']['album']['id'])
 	albums_name.append(i['track']['album']['name'])
 
 all_tracks_uri = []
 
 for album in range(len(albums_uri)):
+
 	set_list = Spotify.album_tracks(albums_uri[album])
+
 	for track in set_list['items']:
 		all_tracks_uri.append(track['id'])
+
 	print('ALBUM:',albums_name[album],'LOADED')
 	
 	
 # Choose random songs
 
+all_tracks_uri = random.sample(all_tracks_uri, len(all_tracks_uri))
+
 chosen = []
+random_num = []
 
 for i in range(100):
+
 	choice = random.randint(0, len(all_tracks_uri))
+
+	while choice in random_num:
+		choice = random.randint(0, len(all_tracks_uri))
+
+	random_num.append(choice)
 	chosen.append(all_tracks_uri[choice])
 
 # Add songs to playlist
 
 Spotify.user_playlist_add_tracks(username, playlist_goal, chosen)
-print('\nDone')
+print('Done')
