6de33a23a2251a84edf38dd504b077b6bb2195b6
manolo <manoloesparta@gmail.com>
2018-11-03 17:03:33 -0700

    update

--- spotify/songxalbum/auth.py
+++ spotify/songxalbum/auth.py
@@ -6,7 +6,7 @@ def auth(username, scope):
 
 	playlist_retrieved = '1ofx1iXeCqb5gPuEWSanfc'
 	playlist_goal = '0iYFyrLsby2E0QBBPs2xWi'
-
+	
 	try:
 		token = util.prompt_for_user_token(username, scope)
 	except:
